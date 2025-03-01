a6f54f70a6d335e2c7e57793d88d07aca9dcf4e3
Anar Qasimov <qasimovknan387@gmail.com>
2025-03-01 08:32:09 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let number1 = 1
+let number2 = 2
+console.log("1+2=" + (number1 + number2))
+console.log("2-1" - (number2 - number1))
+console.log("1*2" * (number1 * number2))
+console.log("2/1" / (number2 / number1))
